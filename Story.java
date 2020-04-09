00f78ef353ebf1b7e5e554e5a402e8db9c186bee
catsarecool45 <63406646+catsarecool45@users.noreply.github.com>
2020-04-09 10:24:58 -0400

    Update Story.java

--- Story.java
+++ Story.java
@@ -1,4 +1,8 @@
 
 public class Story {
-
+  System.out.println("You wake up in a heavy daze. You are sleeping on a worn mattress inside a worn house. You absorb everything in.");
+  System.out.println("The air is cold and silent, at least for the moment. You look to the left and the right and forwards, seeing nothing");
+  System.out.println("but utter destruction. You pause for a while, trying to recall something you can't, but then... the silence is interrupted.");
+  System.out.println("An inhuman roar penatrates the desolate, cold, and dreary atmosphere, a roar that is somewhow familiar. You get up out of the bed");
+  System.out.println("and wait for a sign or hint of what to do next...");     
 }
